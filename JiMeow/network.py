65307e7b12d43af52adec8df79d9f71d426b881f
jiratQ <jirat123@icloud.com>
2022-04-30 21:53:29 +0700

    update collaborators ip

--- JiMeow/network.py
+++ JiMeow/network.py
@@ -1,7 +1,10 @@
 import socket
 import pickle
 
-server = "25.34.159.172"
+
+server = "25.34.159.172"  # JiMeow
+# server = "25.35.236.244" # GolfGrab
+# server = "25.31.231.0" # Minzung
 port = 5555
 
 
--- JiMeow/server.py
+++ JiMeow/server.py
@@ -3,7 +3,10 @@ from threading import *
 import pickle
 from player import Player
 
-server = "25.34.159.172"
+
+server = "25.34.159.172"  # JiMeow
+# server = "25.35.236.244" # GolfGrab
+# server = "25.31.231.0" # Minzung
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
