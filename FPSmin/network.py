f873bbcdafd7b7a08a2ed286b62eb226e9f99cec
Minzung <minza1131@outlook.com>
2022-05-01 20:48:01 +0700

    stash change

--- FPSmin/network.py
+++ FPSmin/network.py
@@ -1,17 +1,15 @@
 import socket
 import pickle
+from threading import Thread
+from time import sleep
 
-# server = "25.34.159.172"  # JiMeow
-# server = "25.35.236.244" # GolfGrab
-server = "25.31.231.0"  # Minzung
-
-
+Minzung = "25.31.231.0"
+JiMeow = "25.34.159.172"
+GolfGrab = "25.35.236.244"
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = server
         self.port = 3000
-        self.addr = (self.server, self.port)
         self.data = self.connect()
         self.id = str(self.data["id"])
         self.pos = self.data["pos"]
@@ -20,6 +18,12 @@ class Network:
         self.client.close()
 
     def connect(self):
+        thread = Thread(target=self.client.connect, args=((Minzung, self.port),))
+        thread.start()
+        sleep(3)
+        if thread.is_alive():
+            pass
+            
         try:
             self.client.connect(self.addr)
             return pickle.loads(self.client.recv(128000))
