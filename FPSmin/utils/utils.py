cf9c0b49959b8e198007df503f05acbeb12fa6e8
Minzung <minza1131@outlook.com>
2022-05-03 20:19:09 +0700

    add draw_text_to_surface function

--- FPSmin/utils/utils.py
+++ FPSmin/utils/utils.py
@@ -1,7 +1,14 @@
 import pygame
 
 
-def load_img(path):
+def load_img(path, colorkey=(0, 0, 0)):
     img = pygame.image.load(path).convert()
-    img.set_colorkey((0, 0, 0))
+    img.set_colorkey(colorkey)
     return img
+
+
+def draw_text_to_surface(surface, font, text, text_color, bg_color, pos):
+    text = font.render(str(text), True, text_color)
+    text_rect = text.get_rect(center=pos)
+    pygame.draw.rect(surface, bg_color, text_rect)
+    surface.blit(text, text_rect)
