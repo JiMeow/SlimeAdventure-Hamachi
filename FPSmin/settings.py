307b675f82c7784acce947ecf2659f8f9bd4a1b0
Minzung <minza1131@outlook.com>
2022-05-04 02:13:56 +0700

    size / 2

--- FPSmin/settings.py
+++ FPSmin/settings.py
@@ -6,8 +6,8 @@ font = pygame.font.Font(None, 20)
 
 
 fps = 60
-width = 1920
-height = 1080
+width = 800
+height = 800
 # make it can load image
 pygame.display.set_mode((width, height))
 background_color = (100, 100, 200)
@@ -17,17 +17,18 @@ player_image_path_green = "FPSmin/assets/green.png"
 player_image_green = load_img(player_image_path_green, gb_colorkey)
 player_image_path_red = "FPSmin/assets/red.png"
 player_image_red = load_img(player_image_path_red, gb_colorkey)
-player_image_size = (100, 100)
-player_normal_speed = 7
+player_image_size = (50, 50)
+player_fast_speed = 8
+player_normal_speed = 5
 player_slow_speed = 3
 player_rotation_speed = 5
 
-cursor_size = (8, 8)
+cursor_size = (4, 4)
 cursor_color = (255, 255, 255)
 # projectile settings
 projectile_image_path = "FPSmin/assets/black.png"
 projectile_image = load_img(projectile_image_path, gb_colorkey)
-projectile_image_size = (10, 10)
+projectile_image_size = (5, 5)
 projectile_speed = 20
 projectile_rotation_speed = 30
 projectile_life_time = 5
