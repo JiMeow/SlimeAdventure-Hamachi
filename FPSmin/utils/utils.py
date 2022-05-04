89173e669d18159ec72522ca70f881c8b824b416
Minzung <minza1131@outlook.com>
2022-05-04 19:18:23 +0700

    move debug to utils

--- FPSmin/utils/utils.py
+++ FPSmin/utils/utils.py
@@ -1,6 +1,10 @@
 import pygame
 import os
 
+pygame.init()
+font_size = 20
+font = pygame.font.Font(None, font_size)
+
 
 def draw_text_to_surface(surface, font, text, text_color, bg_color, pos):
     text = font.render(str(text), True, text_color)
@@ -31,3 +35,12 @@ def create_surface(size, color=None, colorkey=(0, 0, 0)):
     if color:
         surface.fill(color)
     return surface
+
+
+def debug(info, debug_count):
+    screen = pygame.display.get_surface()
+    text = font.render(str(info), True, "White")
+    rect = text.get_rect(topleft=(0, debug_count[0] * (font_size//2+2)))
+    pygame.draw.rect(screen, "Black", rect)
+    screen.blit(text, rect)
+    debug_count[0] += 1
