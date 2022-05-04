6fd15d9f7077cf5ae89703d1006a69f486f1535d
Minzung <minza1131@outlook.com>
2022-05-04 23:08:37 +0700

    tile group

---
+++ FPSmin/tilegroup.py
@@ -0,0 +1,21 @@
+import pygame
+from settings import *
+from tile import Tile
+import random
+
+
+class TileGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+
+    def create_random_tile(self, radius):
+        for x in range(-radius, radius+1, tile_image_size[0]):
+            for y in range(-radius, radius+1, tile_image_size[1]):
+                r = random.randint(0, 255)
+                g = random.randint(0, 255)
+                b = random.randint(0, 255)
+                Tile(
+                    self,
+                    pos=(x, y),
+                    color=(r, g, b),
+                )
