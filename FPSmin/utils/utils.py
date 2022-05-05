089bc3e3b5eced23322721c1aa303043af39cc1d
Minzung <minza1131@outlook.com>
2022-05-05 15:12:15 +0700

    create create_font func adapt draw_text_to_surface create create_key_img func refactor debug

--- FPSmin/utils/utils.py
+++ FPSmin/utils/utils.py
@@ -1,15 +1,25 @@
 import pygame
 import os
 
+
 pygame.init()
-font_size = 20
-font = pygame.font.Font(None, font_size)
 
 
-def draw_text_to_surface(surface, font, text, text_color, bg_color, pos):
+def create_font(font_size=20, font_name=None):
+    font = pygame.font.Font(font_name, font_size)
+    return font
+
+
+def draw_text_to_surface(surface, font, text, text_color="black", bg_color=None, pos=None):
     text = font.render(str(text), True, text_color)
-    text_rect = text.get_rect(center=pos)
-    pygame.draw.rect(surface, bg_color, text_rect)
+    if pos == None:
+        text_rect = text.get_rect(
+            center=(surface.get_width()//2, surface.get_height()//2)
+        )
+    else:
+        text_rect = text.get_rect(center=pos)
+    if bg_color != None:
+        pygame.draw.rect(surface, bg_color, text_rect)
     surface.blit(text, text_rect)
 
 
@@ -29,7 +39,7 @@ def load_ani(path, colorkey=(0, 0, 0), size=None):
     return ani
 
 
-def create_surface(size, color=None, colorkey=(0, 0, 0)):
+def create_surface(size, color=(0, 0, 0), colorkey=(0, 0, 0)):
     surface = pygame.Surface(size).convert()
     surface.set_colorkey(colorkey)
     if color:
@@ -37,10 +47,29 @@ def create_surface(size, color=None, colorkey=(0, 0, 0)):
     return surface
 
 
+def create_key_img(keys, img_size, bg_color, bd_color, center, r, R, font_size, font_name=None, text_color=(0, 0, 0)):
+    key_images = {}
+    for key in keys:
+        surface = create_surface(img_size)
+        pygame.draw.circle(surface, bd_color, center, R)
+        pygame.draw.circle(surface, bg_color, center, r)
+        draw_text_to_surface(
+            surface=surface,
+            font=create_font(font_size, font_name),
+            text=chr(key).upper(),
+            text_color=text_color,
+            bg_color=None,
+            pos=None,
+        )
+        key_images[key] = surface
+    return key_images
+
+
 def debug(info, debug_count):
     screen = pygame.display.get_surface()
+    font = create_font(20)
     text = font.render(str(info), True, "White")
-    rect = text.get_rect(topleft=(0, debug_count[0] * (font_size//2+2)))
+    rect = text.get_rect(topleft=(0, debug_count[0] * (20//2+2)))
     pygame.draw.rect(screen, "Black", rect)
     screen.blit(text, rect)
     debug_count[0] += 1
