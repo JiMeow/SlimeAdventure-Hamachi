5ec0922c9457474557a76cd561ac32e0567ed276
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2022-12-13 14:54:08 +0700

    add hostIp.txtt

--- src/setting.py
+++ src/setting.py
@@ -1,6 +1,9 @@
 # server = "25.34.159.172"  # Input server IP of you hamachi host here
-server = "127.0.0.1"  # Localhost
+# server = "127.0.0.1"  # Localhost
 port = 5555
 width = 1300
 height = 900
 floor = 690
+
+with open("hostIP.txt", "r") as f:
+    server = f.read()
